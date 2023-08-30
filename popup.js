482b2b46f149e86b91bc3c878f36cc4a577fedb1
Yousef Al-Wahami <138406804+wahamiyousef@users.noreply.github.com>
2023-08-30 13:22:56 -0400

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,18 +1,30 @@
-const button = document.querySelector(".button");
-const circle = document.querySelector(".circle");
+if(document.querySelector(".popup")){
 
-let buttonOn = false;
+    const button = document.querySelector(".button");
+    const circle = document.querySelector(".circle");
 
-button.addEventListener("click", ()=>{
-    //if buttonOn is false
-    if(!buttonOn){
-        buttonOn=true;
-        circle.style.animation = "moveCircleRight 1s forwards";
-        button.style.animation = "backgroundLight 1s forwards";
-    }
-    else{
-        buttonOn=false;
-        circle.style.animation = "moveCircleLeft 1s forwards";
-        button.style.animation = "backgroundDark 1s forwards";
-    }
-})
+    let buttonOn = false;
+
+    button.addEventListener("click", ()=>{
+        // if buttonOn is false
+        if(!buttonOn){
+            buttonOn=true;
+            circle.style.animation = "moveCircleRight 1s forwards";
+            button.style.animation = "backgroundLight 1s forwards";
+            chrome.scripting.executeScript({
+                // execute script on selected tab
+                // clean up code by making new js file
+                file: "appOn.js"
+            })
+        }
+        else{
+            buttonOn=false;
+            circle.style.animation = "moveCircleLeft 1s forwards";
+            button.style.animation = "backgroundDark 1s forwards";
+            chrome.tabs.executeScript({
+                file: "appOff.js"
+            })
+        }
+    })
+
+}
